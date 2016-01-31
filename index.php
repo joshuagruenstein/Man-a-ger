4d7842f1ef5c83ba08b23c9ffc4168927ef92b6d
Luca Koval <lucakoval@me.com>
2016-01-31 09:10:32 -0500

    tables
    yee

--- index.php
+++ index.php
@@ -18,7 +18,30 @@
 
 <body>
 	<div class="container">
-		
+		<div class="pageContent">
+			<div class="row">
+				<div class="col-sm-8">
+					<div class="panel panel-primary">
+						<div class="panel-heading">
+							<h3 class="panel-title">Products</h3>
+						</div>
+						<table class="table well well-sm" id="leaderTable">
+							<thead>
+								<tr>
+									<th>Name of Product</th>
+								</tr>
+							</thead>
+							<tbody id="products">
+								<tr>
+									<th>Apples</th>
+									<th>Milk</th>
+									<th>Oranges</th>
+								</tr>
+							</tbody>
+						</table>
+					</div>
+				</div>
+		</div>
 	</div>
 
 	<?php include 'includes/footer.php'; ?>
