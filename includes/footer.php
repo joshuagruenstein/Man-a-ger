958423502997075766350310946bc7a900e7db5e
Luca Koval <lucakoval@me.com>
2016-01-31 08:53:56 -0500

    header and footer
    yee

---
+++ includes/footer.php
@@ -0,0 +1,38 @@
+<footer class="footer pageContent">
+	<div class="container">
+		<div id="footer">
+			<ul class="pager">
+				<li class="previous" id="backButton"><a href="#"><span aria-hidden="true">&larr;</span> Older</a></li>
+				<li><a href="#" data-toggle="modal" data-target="#aboutModal">About</a></li>
+				<li class="next" id="nextButton"><a href="#">Newer <span aria-hidden="true">&rarr;</span></a></li>
+			</ul>
+		</div>
+	</div>
+</footer>
+
+<div class="modal fade" id="aboutModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
+	<div class="modal-dialog" role="document">
+		<div class="modal-content">
+			<div class="modal-header">
+				<button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+				<h4 class="modal-title" id="myModalLabel">About the New York Computer Science League</h4>
+			</div>
+			<div class="modal-body">
+				<p>NYCSL, or the New York Computer Science League, is a programming competition created for New York high schools students to compete against each-other while solving algorithmic computer science problems.</p>
+				<p>Each month at midnight a new challenge is posted.  Programmers in NYCSL have one month to upload as many solutions as they like; only the top score is counted.  Solutions are instantly graded and put up on both global and school-wide leaderboards.  Problems are designed such that they are suitable for programmers of all skill levels; beginners are encouraged to participate.<p>
+				<p>Currently the following schools are supported. If you'd like your school to be added, please contact us.
+					<ul>
+						<li>Horace Mann School</li>
+						<li>Riverdale Country School</li>
+						<li>Ethical Culture Fieldston School</li>
+						<li>Trinity School</li>
+						<li>Dalton School</li>
+						<li>Stuyvesant High School</li>
+						<li>Bronx High School of Science</li>						
+					</ul>
+				</p>
+				<p>NYCSL.io was created by programmers  <a href="https://github.com/truell20">Michael Truell</a>, <a href="https://github.com/joshuagruenstein">Josh Gruenstein</a>, <a href="https://github.com/G4cool">Luca Koval</a>, and <a href="https://github.com/Sydriax">Ben Spector</a> from Horace Mann during the defhacks("Winter",2015) hackathon at Facebook NY.  The code is available on Github <a href="https://github.com/BackPropagation/NYCSL">here</a>.  You may contact us at <a href="mailto:contact@nycsl.io">contact@nycsl.io</a>.
+			</div>
+		</div>
+	</div>
+</div>
---
+++ includes/navbar.php
@@ -0,0 +1,106 @@
+<nav class="navbar navbar-default" id="navvy">
+	<div class="container-fluid">
+		<div class="navbar-header">
+			<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+				<span class="sr-only">Toggle navigation</span>
+				<span class="icon-bar"></span>
+				<span class="icon-bar"></span>
+				<span class="icon-bar"></span>
+			</button>
+			<a class="navbar-brand" href="/">NYCSL</a>
+		</div>
+		<div id="navbar" class="navbar-collapse collapse">
+			<ul class="nav navbar-nav">
+				<li class="dropdown">
+					<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Schools<span class="caret"></span></a>
+					<ul class="dropdown-menu" id="schoolsDropdown">
+						
+					</ul>
+				</li>
+			</ul>
+			<ul class="nav navbar-nav navbar-right loggedOut" id="loginNav">
+				<ul class="nav navbar-nav">
+					<li class="dropdown">
+						<a class="dropdown-toggle" href="#" data-toggle="dropdown">Login<strong class="caret"></strong></a>
+						<ul id="loginForm" class="dropdown-menu" style="padding: 10px; padding-bottom: 0px;">
+							<div class="form-group label-floating is-empty">
+								<label for="login_user" class="control-label">Email</label>
+								<input type="email" class="form-control" id="login_user">
+								<input type="submit" style="display: none;">
+							</div>
+
+							<div class="form-group label-floating is-empty">
+								<label for="login_pass" class="control-label">Password</label>
+								<input id="login_pass" class="form-control" type="password" size="30" >
+								<span class="material-input"></span>
+							</div>
+							<input id="loginButton" class="btn btn-primary" style="clear: left; width: 100%; height: 32px; font-size: 13px; margin-bottom:0px" type="submit" name="commit" value="Login" />
+							<button class="btn btn-secondary" data-toggle="modal" data-target="#forgotModal" style="clear: left; width: 100%; height: 32px; font-size: 13px; margin-bottom:15px">Forgot</button>
+						</ul>
+					</li>
+
+					<li class="dropdown">
+						<a class="dropdown-toggle" href="#" data-toggle="dropdown">Register<strong class="caret"></strong></a>
+						<ul id="registerForm" class="dropdown-menu" style="padding: 15px; padding-bottom: 0px;">
+							<div class="form-group label-floating is-empty">
+								<label for="register_first" class="control-label">First Name</label>
+								<input id="register_first" class="form-control" type="text" size="30" >
+								<span class="material-input"></span>
+							</div>
+							<div class="form-group label-floating is-empty">
+								<label for="register_last" class="control-label">Last Name</label>
+								<input id="register_last" class="form-control" type="text" size="30" >
+								<span class="material-input"></span>
+							</div>
+							<div class="form-group label-floating is-empty">
+								<label for="register_email" class="control-label">Email</label>
+								<input id="register_email" class="form-control" type="email" size="30" >
+								<span class="help-block" id="schoolField">Enter your school email.</span>
+								<span class="material-input"></span>
+							</div>
+							<div class="form-group label-floating is-empty">
+								<label for="register_pass" class="control-label">Password</label>
+								<input id="register_pass" class="form-control" type="password" size="30" >
+								<span class="material-input"></span>
+							</div>
+							<input id="registerButton" class="btn btn-primary" style="clear: left; width: 100%; height: 32px; font-size: 13px; margin-bottom:15px" type="submit" name="commit" value="Register" />
+						</ul>
+					</li>
+				</ul>
+			</ul>
+			<form id="submitForm">
+				<ul class="nav navbar-nav navbar-right loggedIn" id="logoutNav">
+					<li><a href="#" id="submitButton">Submit To Current Competition</a><input type="file" id="myFile" name="outputFile" onchange="fileChanged()"></li>
+					<li><a href="#" id="logoutButton">Logout</a></li>
+				</ul>
+			</form>
+		</div>
+	</div>
+</nav><div id="messageBox"></div>
+
+<div class="modal fade" id="forgotModal" tabindex="-1" role="dialog" aria-labelledby="forgotModal">
+	<div class="modal-dialog" role="document">
+		<div class="modal-content">
+			<div class="modal-header">
+				<button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+				<h3 class="modal-title" id="myModalLabel">Password Recovery</h3>
+			</div>
+			<div class="modal-body" >
+					<div class="alert alert-danger" role="alert" id="noEmailRecoveryAlert" style="display:none">
+						<span class="glyphicon glyphicon-exclamation-sign" aria-hidden="true"></span>
+						<span class="sr-only">Error:</span>
+						Email could not be found in our database.
+					</div>
+					<p>Please enter your school email below and we will send you a recovery link.</p>
+					<div class="form-group label-floating is-empty" style="text-align:left">
+						<label for="forgotMail" class="control-label">Email</label>
+						<input id="forgotMail" class="form-control" type="email" size="30" >
+						<span class="material-input"></span>
+					</div>
+					<div style="text-align:center;width:100%">
+						<button id="recoverButton" type="submit" class="btn btn-primary">Recover</button>
+					</div>
+			</div>
+		</div>
+	</div>
+</div>
