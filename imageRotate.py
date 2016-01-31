c0fc35c66beac9024214beef6e53b0432328ad93
Luca Koval <lucakoval@me.com>
2016-01-31 12:08:14 -0500

    image rotation works
    yee

--- imageRotate.py
+++ imageRotate.py
@@ -5,7 +5,7 @@ with Image(filename='mona-lisa.png') as img:
     print(img.size)
     for r in 1, 2, 3:
         with img.clone() as i:
-            i.resize(int(i.width * r * 0.25), int(i.height * r * 0.25))
+            # i.resize(int(i.width * r * 0.25), int(i.height * r * 0.25))
             i.rotate(90 * r)
             i.save(filename='mona-lisa-{0}.png'.format(r))
             display(i)
