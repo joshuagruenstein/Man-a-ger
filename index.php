ea849de42252492c8062644e5bfafdbaa19f2468
truell20 <truell20@gmail.com>
2016-01-31 12:35:39 -0500

    Changes to frontend

--- index.php
+++ index.php
@@ -28,8 +28,48 @@
 					    	<button class="btn btn-default" type="button">Enter</button>
 					    </span>
 					</div>
-					<ul class="list-group" id="inFridgeList">
+					<ul class="list-group">
+						<li class="list-group-item">
+							<span class="label label-default label-pill pull-xs-left">9</span>
+					    	Apple
+					    	<div class="pull-right"><span class="glyphicon glyphicon-remove"></span></div>
+					  	</li>
+					  	<li class="list-group-item">
+					    	<span class="label label-default label-pill pull-xs-left">2</span>
+					    	Orange
+					    	<div class="pull-right"><span class="glyphicon glyphicon-remove"></span></div>
+					  	</li>
+					  	<li class="list-group-item">
+					    	<span class="label label-default label-pill pull-xs-left">1</span>
+					    	Milk
+					    	<div class="pull-right"><span class="glyphicon glyphicon-remove"></span></div>
+					  	</li>
 					</ul>
+
+					<div class="row">
+						<div class="col-sm-2">
+							<div class="dropdown" id="itemDropdown">
+						  		<button class="btn btn-default dropdown-toggle" type="button" id="dropdownMenu1" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+						    		Items
+						    		<span class="caret"></span>
+						  		</button>
+						  		<ul class="dropdown-menu" aria-labelledby="dropdownMenu1">
+							    	<li><a href="#">Apple</a></li>
+							    	<li><a href="#">Orange</a></li>
+							    	<li><a href="#">Milk</a></li>
+								</ul>
+							</div>
+						</div>
+						<div class="col-sm-10">
+							<div class="input-group" id="quantityInput">
+								<span class="input-group-addon" id="basic-addon2">Quantity</span>
+								<input type="text" class="form-control" placeholder="3" aria-describedby="basic-addon2">
+								<span class="input-group-btn">
+							    	<button class="btn btn-default" type="button">Enter</button>
+							    </span>
+							</div>
+						</div>
+					</div>
 				</div>
 				<div class="col-sm-5" id="nutritionColumn">
 					<div class="panel panel-primary" id="nutritionPanel">
@@ -51,8 +91,44 @@
 						</table>
 
 						<div class="text-center" id="chartContainer">
-							<canvas id="canvas" width="400" height="400"></canvas>
+							<canvas id="canvas" width="200%" height="200%"></canvas>
 						</div>
+					</div>
+
+					<div class="panel panel-primary" id="weeklyPanel">
+						<div class="panel-heading">
+							<h3 class="panel-title">Weekly Values</h3>
+						</div>
+
+						<table class="table well well-sm" id="weeklyTable">
+							<thead>
+								<tr>
+									<th>Name</th>
+									<th>Percent</th>
+								</tr>
+							</thead>
+							<tbody id="weeklyInfo">
+								<tr>
+									<th>Calories</th>
+									<th>22</th>
+								</tr>
+								<tr>
+									<th>Sodium</th>
+									<th>15</th>
+								</tr>
+								<tr>
+									<th>Vitamin A</th>
+									<th>67</th>
+								</tr>
+							</tbody>
+						</table>
+
+						<nav id="weeklyNav">
+							<ul class="pager">
+						    	<li class="previous"><a href="#"><span aria-hidden="true">&larr;</span> Older</a></li>
+						    	<li class="next"><a href="#">Newer <span aria-hidden="true">&rarr;</span></a></li>
+							</ul>
+						</nav>
 					</div>
 				</div>
 			</div>
@@ -61,10 +137,9 @@
 
 	<?php include 'includes/footer.php'; ?>
 
+	<script src="js/customCharts.js"></script>
 	<script src="lib/jquery.min.js"></script>
 	<script src="lib/bootstrap.min.js"></script>
-	<script src="js/backend.js"></script>
 	<script src="lib/Chart.js"></script>
-	<script src="js/index.js"></script>
 </body>
 </html>
