26d94f527db7770b8021fa6d0835083c26d13d9a
Luca Koval <lucakoval@me.com>
2016-01-31 15:57:43 -0500

    fixed modal
    yee

--- includes/footer.php
+++ includes/footer.php
@@ -16,8 +16,8 @@
 				<h4 class="modal-title" id="myModalLabel">About Kitchen Manager</h4>
 			</div>
 			<div class="modal-body">
-				<p>Man(a)ger is a tool developed to help people with their daily lives.<p>
-				<p>Man(a)ger was created by programmers  <a href="https://github.com/truell20">Michael Truell</a>, <a href="https://github.com/joshuagruenstein">Josh Gruenstein</a>, <a href="https://github.com/G4cool">Luca Koval</a>, and <a href="https://github.com/Sydriax">Ben Spector</a> from Horace Mann during the HackTrin hackathon.  The code is available on Github <a href="https://github.com/BackPropagation/NYCSL">here</a>.  You may contact us at <a href="#">contact@kitchenmanager</a>.
+				<p>Man(a)ger keeps tabs on what happens in your kitchen and provides you with analytics regarding how your household is eating. When you buy or consume food, simply wave it past Man(a)ger and it'll add it to its database. Later, head online and find what percentage of your food consumption is sugars, protein, fats, etc. Manager will also automatically reorder food when you run out of it, making restocking your home easier than ever.<p>
+				<p>Man(a)ger was created by programmers  <a href="https://github.com/truell20">Michael Truell</a>, <a href="https://github.com/joshuagruenstein">Josh Gruenstein</a>, <a href="https://github.com/G4cool">Luca Koval</a>, and <a href="https://github.com/Sydriax">Ben Spector</a> from Horace Mann during the HackTrin hackathon.
 			</div>
 		</div>
 	</div>
