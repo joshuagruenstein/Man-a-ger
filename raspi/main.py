c8abc766bd2a657791ed860d8b4d4a757afd0133
truell20 <truell20@gmail.com>
2016-01-31 10:14:22 -0500

    python inser

---
+++ raspi/main.py
@@ -0,0 +1,16 @@
+import pymysql.cursors
+import requests
+import base64
+import hashlib
+import hmac
+import json
+
+def make_auth_token(upc_string, auth_key):
+	sha_hash = hmac.new(auth_key, upc_string, hashlib.sha1)
+	return base64.b64encode(sha_hash.digest())
+
+def storeProductInfo(upcCode):
+	r = requests.get("https://www.digit-eyes.com/gtin/v2_0/", params={'upcCode': upcCode, 'field_names': 'all', 'language': 'en', 'app_key': '/7917Xl9IRJS', 'signature': make_auth_token(upcCode.encode('utf-8'), "Gc08V4m0s2Ru4Gy1".encode('utf-8')).decode("utf-8")})
+	print(r.json())
+	requests.post("http://162.243.36.95/php/product", data={"productID": upcCode, ""})
+storeProductInfo("0041143020104")
