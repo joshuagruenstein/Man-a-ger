f34825092228c5350a06f1e0de7ee55b02765b35
Luca Koval <lucakoval@me.com>
2016-01-31 08:51:11 -0500

    more index.php
    yay

---
+++ index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+
+	<meta name="description" content="">
+	<meta name="author" content="">
+	<link rel="shortcut icon" href="img/favicon.ico" />
+
+	<title>Kitchen Manager</title>
+
+	<link href="lib/bootstrap.min.css" rel="stylesheet">
+
+	<!-- <link href="style/general.css" rel="stylesheet"> -->
+</head>
+
+<body>
+	<div class="container">
+		<?php include 'includes/navbar.php'; ?>
+
+		<?php include 'includes/footer.php'; ?>
+	</div>
+
+	<script src="lib/jquery.min.js"></script>
+	<script src="lib/bootstrap.min.js"></script>
+</body>
+</html>
