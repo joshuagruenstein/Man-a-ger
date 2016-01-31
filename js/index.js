7c5558320e3c774723a72c61067a5c4321f083c2
joshuagruenstein <joshuagruenstein@gmail.com>
2016-01-31 15:57:26 -0500

    confirmation

--- js/index.js
+++ js/index.js
@@ -124,5 +124,6 @@ $(document).ready(function() {
 		var productUPC = $("#basic-addon2").attr("UPC")
 		var quantity = $("#orderQuantity").val()
 		submitOrder(productUPC,quantity)
+		$("#orderColumn").after('<div class="alert alert-success alert-dismissible" role="alert"><button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button><strong>Congratulations!</strong> Your order has been placed successfully.</div>');
 	})
 })
