cf39fae120fe9a26cc66f6c2beae6c1475e44ecb
Joshua Gruenstein <joshuagruenstein@gmail.com>
2016-01-31 19:33:23 +0000

    barcode

--- raspi/image.py
+++ raspi/image.py
@@ -1,15 +1,25 @@
 import pygame
 import pygame.image
 import pygame.camera
-import subproccess
+import subprocess
+import string
 
 pygame.init()
 pygame.camera.init()
 cam = pygame.camera.Camera("/dev/video0",(640,480))
 cam.start()
 
-while True:
+def getBarcode():
 	image = cam.get_image()
-	image.save("TestCapture.png")
+	pygame.image.save(image, "temp.png")
+	p = subprocess.Popen("/usr/bin/zbarimg temp.png", stdout = subprocess.PIPE, stderr = subprocess.PIPE,shell=True)
+	result, errors = p.communicate()
+	answer = ""
+	if result[:3] == "EAN" or result[:3] == "UPC":
+		answer = result[string.find(result, ":") + 1 : string.find(result, "\n")]
+	else:
+		answer =  "fail"
+	print(answer)
+	return answer
 
-	#os.system("")
+
